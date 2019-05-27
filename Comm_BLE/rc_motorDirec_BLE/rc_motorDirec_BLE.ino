002b54bb34c3862393ae3079548fb5ce9bd78289
Soo-yeon <jsjs331@naver.com>
2019-05-27 18:49:20 +0900

    Direc_last

--- Comm_BLE/rc_motorDirec_BLE/rc_motorDirec_BLE.ino
+++ Comm_BLE/rc_motorDirec_BLE/rc_motorDirec_BLE.ino
@@ -6,9 +6,20 @@ int in1 = 9;
 int in2 = 6;
 int in3 = 10;
 int in4 = 11;
- 
-int RSpeed = 255;
-int LSpeed = 255;
+
+//직진
+int RSpeed = 145;
+int LSpeed = 149;
+
+//회전
+int RSpeed2 = 90;
+int LSpeed2 = 90;
+
+//후진
+int RSpeed3 = 130;
+int LSpeed3 = 148;
+
+
  
 char data;
  
@@ -28,21 +39,21 @@ void loop() {
     }
     if (data == 'r') { //우회전
       analogWrite(in1, 0);
-      analogWrite(in2, 0);
+      analogWrite(in2, RSpeed2);
       analogWrite(in3, LSpeed);
       analogWrite(in4, 0);
     }
     if (data == 'l') { //좌회전
       analogWrite(in1, 0);
       analogWrite(in2, RSpeed);
-      analogWrite(in3, 0);
+      analogWrite(in3, LSpeed2);
       analogWrite(in4, 0);
     }
     if (data == 'b') { //후진
-      analogWrite(in1, RSpeed);
+      analogWrite(in1, RSpeed3);
       analogWrite(in2, 0);
       analogWrite(in3, 0);
-      analogWrite(in4, LSpeed);
+      analogWrite(in4, LSpeed3);
     }
     if (data == 's') { //정지
       analogWrite(in1, 0);
